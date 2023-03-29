7db27532636803219d2ff1140d06a704cc5206bb
JamesYu <115390358+Yutian2000@users.noreply.github.com>
2023-03-29 17:31:16 +0800

    Create mutation-types.js

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,6 +1,6 @@
 //AI头像地址设置
 export const AI_HEAD_IMG_URL="https://th.bing.com/th?id=ODL.3e2fbff4543f0d3632d34be6d02adc93&w=100&h=100&c=12&pcl=faf9f7&o=6&dpr=1.5&pid=13.1"
 //用户头像地址设置
-export const USER_HEAD_IMG_URL="https://avatars.githubusercontent.com/u/40659515?v=4"
+export const USER_HEAD_IMG_URL="https://github.com/Yutian2000/ChatGPT_JCM/blob/main/src/assets/img/Cyberpunk_android_bodyhttpsmedia.discordapp.netattachments10_827d8449-b4c1-4e7d-9d05-25fb61a3520b.png"
 //用户名称设置
 export const USER_NAME=""
