ec44a21fe90f73f76c2bab0b7e7aa771244a0e43
JamesYu <115390358+Yutian2000@users.noreply.github.com>
2023-03-29 17:21:38 +0800

    Update setting.vue

--- src/view/pages/setting.vue
+++ src/view/pages/setting.vue
@@ -4,7 +4,7 @@
       <el-header>
         <transition name="el-zoom-in-top">
           <div v-show="show" class="transition-box">
-            <h3>L V S H I H A O</h3>
+            <h3>JamesYu</h3>
           </div>
         </transition>
       </el-header>
@@ -12,11 +12,11 @@
         <transition name="el-zoom-in-top">
           <div v-show="show" class="transition-box">
             <span>
-              很感谢大家对我的支持，现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API后续会添加更多有意思的功能进去，希望大家给我的GitHub点个小小的星星，
-              大家如果有什么好的想法可以在GitHub中提出来，我们一起来完善这个页面。
+              很感谢大家对我的支持，现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API后续会添加更多有意思的功能进去，
+              大家如果有什么好的想法可以关注微信公众号：【趣盈全球】中提出来。下面是两个不同风格的ChatGPT聊天页面，欢迎你们的体验！
             </span>
             <div>
-              <a href="https://space.bilibili.com/326625155?spm_id_from=333.337.0.0">BliBili</a>---<a href="https://github.com/202252197/ChatGPT_JCM">GitHub</a>
+              <a href="https://chatgpt.ququinc.com/">精简版</a>---<a href="https://ai.ququcloud.com/">高级版</a>
             </div>
           </div>
         </transition>
